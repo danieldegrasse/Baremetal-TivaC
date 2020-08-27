72e2d9e83eedc220f22fe33b0fcab812267e1c71
Daniel DeGrasse <daniel@degrasse.com>
2020-08-27 01:32:54 -0500

    Implemenented all functionality of blink.c in assembly

--- loop.s
+++ loop.s
@@ -4,44 +4,70 @@
 .global _entry @export the entry point
 _entry:
     @ First, enable clock gating on RCGCGPIO
-    LDR r0, SYSCTL_RCCGPIO @load value of SYSCTL_RCGCGPIO symbol into r0
-    LDR r1, [r0] @load value of RCGCGPIO
-    MOV r2, #32
-    ORR r2, r2, r1 @ logical OR with BIT5
-    STR r2, [r0]
-    LDR r0, SYSCTL_PRGPIO
+    LDR r4, SYSCTL_RCCGPIO @load value of SYSCTL_RCGCGPIO symbol into r4
+    LDR r5, [r4] @load value of RCGCGPIO
+    MOV r6, #32
+    ORR r6, r6, r5 @ logical OR with BIT5
+    STR r6, [r4]
+    LDR r4, SYSCTL_PRGPIO
 _GATE_LOOP:
-    LDR r1, [r0]
-    MOV r2, #32
-    AND r2, r2, r1
-    CBNZ r2, _GATE_EXIT @ if (32 & SYSCTLPRGPIO) was not zero, branch out
+    LDR r5, [r4]
+    MOV r6, #32
+    AND r6, r6, r5
+    CBNZ r6, _GATE_EXIT @ if (32 & SYSCTLPRGPIO) was not zero, branch out
     B _GATE_LOOP
 _GATE_EXIT:
-    MOV r3, #1 @set r3 so CBZ does not jump
-    LDR r0, GPIOFDIR
+    MOV r7, #1 @set r7 so CBZ does not jump
+    LDR r4, GPIOFDIR
 _SET_BIT1:
-    @ This snippet sets BIT1 of the memory r0's value points to
-    LDR r1, [r0] @load value based off r0
-    MOV r2, #2 @BIT1 (for PF.1 GPIO PIN)
-    ORR r2, r2, r1
-    STR r2, [r0]
-    CBZ r3, _BLINK @ if r3 is zero, jump to blink routine
+    @ This snippet sets BIT1 of the memory r4's value points to
+    LDR r5, [r4] @load value based off r4
+    MOV r6, #2 @BIT1 (for PF.1 GPIO PIN)
+    ORR r6, r6, r5
+    STR r6, [r4]
+    CBZ r7, _BLINK @ if r7 is zero, jump to blink routine
     @ Enable GPIO PIN
-    LDR r0, GPIOFDEN
-    MOV r3, #0 @now CBZ will jump
+    LDR r4, GPIOFDEN
+    MOV r7, #0 @now CBZ will jump
     B _SET_BIT1 @ jump back to set bit snippet
 _BLINK:
-    ldr r0, GPIOFDATA
-    mov r1, #2
-    str r1, [r0]
-    b . @ Infinite loop here (branch to our instruction)
+    LDR r4, GPIOFDATA
+_BLINK_L:
+    MOV r5, #2
+    STR r5, [r4]
+    LDR r0, DELAY_LEN @ set argument for function call
+    BL _DELAY 
+    @ Now zero out GPIOFDATA to turn off LED
+    MOV r5, #0
+    STR r5, [r4]
+    LDR r0, DELAY_LEN @ set argument for function cal
+    BL _DELAY 
+    b _BLINK_L @ jump back to start of blink sequence
+
+
+@ This subroutine delays for "r0"*3 clock cycles
+_DELAY:
+    @ First, save the registers (like a function would)
+    PUSH {r4-r7}
+    MOV r4, r0 @copy r0 to r4
+_DELAY_LOOP:
+    SUB r4, r4, #1 @decrement by 1
+    CBZ r4, _DELAY_RET @ break if r4 is zero
+    B _DELAY_LOOP @ otherwise keep looping
+_DELAY_RET:
+    POP {r4-r7}
+    BX lr @ return back to caller
+
+
+
+
 
     @ A nice little NOP sled to keep the data after this word aligned.    
-    NOP
 
 SYSCTL_RCCGPIO: .word 1074783752
 SYSCTL_PRGPIO: .word 1074784776
 GPIOFDATA: .word 1073893384
 GPIOFDIR: .word 1073894400
 GPIOFDEN: .word 1073894684
+DELAY_LEN: .word 500000
 
